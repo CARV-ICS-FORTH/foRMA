b4163b38b4106177f148b0fd9a1d65200c7748b9
kanellou <lena.kanellou@gmail.com>
2022-10-04 15:16:45 +0300

    - inserted checks for window lookaside translation buffer, they are mostly commented out now - profiler copies the trace.windows attribute of each trace instance into the relevant field of a list named windows

--- dumpifile.py
+++ dumpifile.py
@@ -88,12 +88,15 @@ class MyTrace(DumpiTrace):
 		# count mpi_win_fence occurrences
 		self.fence_count += 1
 
+		## identify window key to use on windows dictionary by looking into wintb
+		win_id = self.wintb[data.win]
+
 		## elements of self.windows[data.win] (value) are: window size [0], current window epoch [1], list of bytes moved per epoch [2]
 
 		## increase epoch count on corresponding window
 		## first fence ever on 
-		self.windows[data.win][1]+=1
-
+		# self.windows[data.win][1]+=1
+		self.windows[win_id][1] += 1
 
 		"""if (self.windows[data.win][1]>0):
 			print(f'win fence on window {data.win}: Fence count is {self.fence_count} | Epoch (completed) count on window is {self.windows[data.win][1]}')
@@ -101,8 +104,10 @@ class MyTrace(DumpiTrace):
 
 		## prepare dictionary for next iteration. i.e.:
 		## add a zero element to list of bytes moved per epoch in order to save on a check on whether that epoch exists later on (in on_get, on_put, for ex.)
-		if (self.windows[data.win][1]>-1): ## epoch count in self.windows[data.win][1] has already been incremented, so the >-1 check is correct
-			self.windows[data.win][2].append(0)
+		# if (self.windows[data.win][1]>-1): ## epoch count in self.windows[data.win][1] has already been incremented, so the >-1 check is correct
+		if (self.windows[win_id][1]>-1): 
+			# self.windows[data.win][2].append(0)
+			self.windows[win_id][2].append(0)
 			# since I have no way of knowing whether the current fence call is the last one in the execution, 
 			# there will always be a last, 0-value field in this list. so, the length of the list is epoch#+1
 		#print(f'window {data.win} : current data volumes per epoch are: {self.windows[data.win]}')
@@ -133,40 +138,47 @@ class MyTrace(DumpiTrace):
 
 		# elements of self.windows[data.win] (value) are: window size, current window epoch, list of bytes moved per epoch
 		# initializing data volume of first epoch to zero anyway
-		self.windows[data.win] = [data.size, -1, []]
+		#self.windows[data.win] = [data.size, -1, []]
+		self.windows[self.wintb[data.win]] = [data.size, -1, []]
 
 	def on_win_free(self, data, thread, cpu_time, wall_time, perf_info):
 		self.wintb[data.win] = -1
 
 	def on_get(self, data, thread, cpu_time, wall_time, perf_info):
 		time_diff = wall_time.stop - wall_time.start
+		win_id = self.wintb[data.win]
 		#print(f'on_get window: {data.win}')
 		## when in doubt, check out pydumpi/dtypes.py
 		#print(f'on_get count: {data.origincount}')
 		#print(f'on_get data type: {data.origintype}')
 		#print(f'on_get size in bytes: {data.origincount*self.type_sizes[data.origintype]}')
 		self.windows[data.win][2][self.windows[data.win][1]]+=data.origincount*self.type_sizes[data.origintype]
+		#self.windows[win_id][2][self.windows[win_id][1]]+=data.origincount*self.type_sizes[data.origintype]
 
 
 	def on_put(self, data, thread, cpu_time, wall_time, perf_info):
 		time_diff = wall_time.stop - wall_time.start
+		win_id = self.wintb[data.win]
 		#print(f'on_put window: {data.win}')
 		## when in doubt, check out pydumpi/dtypes.py
 		#print(f'on_put count: {data.origincount}')
 		#print(f'on_put data type: {data.origintype}')
 		#print(f'on_put size in bytes: {data.origincount*self.type_sizes[data.origintype]}')
 		self.windows[data.win][2][self.windows[data.win][1]]+=data.origincount*self.type_sizes[data.origintype]
+		#self.windows[win_id][2][self.windows[win_id][1]]+=data.origincount*self.type_sizes[data.origintype]
 
 
 	def on_accumulate(self, data, thread, cpu_time, wall_time, perf_info):
 		time_diff = wall_time.stop - wall_time.start
+		win_id = self.wintb[data.win]
 		#print(f'on_acc window: {data.win}')
 		## when in doubt, check out pydumpi/dtypes.py
 		#print(f'on_acc count: {data.origincount}')
 		#print(f'on_acc data type: {data.origintype}')
 
 		## TODO: refine this callback, depending on acc operation and transfer direction... :/
-		self.windows[data.win][2][self.windows[data.win][1]]+=data.origincount*self.type_sizes[data.origintype]
+		#self.windows[data.win][2][self.windows[data.win][1]]+=data.origincount*self.type_sizes[data.origintype]
+		self.windows[win_id][2][self.windows[win_id][1]]+=data.origincount*self.type_sizes[data.origintype]
 
 
 def parse_traces():
@@ -199,6 +211,8 @@ def parse_traces():
 
 
 	windows = [None for x in range(totalRanks)]
+	windowsums = [None for x in range(totalRanks)]
+
 
 	rank = 0
 
@@ -213,11 +227,37 @@ def parse_traces():
 	        print(f'Fence count for rank {rank} is: {trace.fence_count}')
 	        print(f'Win_create occurrences for rank {rank} is: {trace.wincreate_count}')
 
-	        print(f'Different win IDs for rank {rank} is: {len(trace.windows)}')
-	        print(f'Win IDs for rank {rank} are: {(trace.windows).keys()}')
-	        
+	        #print(f'Different win IDs for rank {rank} is: {len(trace.windows)}')
+	        #print(f'Win IDs for rank {rank} are: {(trace.windows).keys()}')
+
+
+
+
 	        #print(windows)
 	        rank+=1
+
+
+
+
+
+	length = len(windows[0])
+	prev_fences = 0
+	for win in range(length):
+		print(f'Window key is {win+1}, size is {windows[0][win+1][0]}')
+		prev_rank_val = 0
+		for rank in range(totalRanks):
+			if rank > 0:
+				if prev_rank_val != windows[rank][win+1][0]:
+					print(f'size discrepancy on window {win+1} for rank {rank}!!!')
+			prev_rank_val = windows[rank][win+1][0]
+
+			
+			"""
+			if rank == 0:
+				if prev_fences != windows[rank][win+1][1]:
+					print(f'rank {rank}: epochs differ between windows {win} ({prev_fences}) and {win+1} ({windows[rank][win+1][1]})')
+			prev_fences = windows[rank][win+1][1]
+			"""
 
 
 
