Fixed printing output so that times are in usec. Changed calculation of data transfer bounds for more memory efficiency: now epoch DT bound stats are calculated on-the-fly by using the operation start timestamp for finding min and max and adjusting at the fence for the actual duration. Then forma_merge_stats_x4 is used at the fence for trace-level summary calculation.

forma_classes.py
@@ -172,23 +172,23 @@ class formaSummary:
 		## are necessary 
 		overalls = np.stack((self.exectime, self.rmatime))
 		print('------------------------------------------------------------------------------------------\n' +
-		'------------------------ [Operation] Durations (nsec) ------------------------------------\n')
+		'------------------------ [Operation] Durations (usec) ------------------------------------\n')
 		fp.forma_print_stats_x6(["Total exec. time", "Total time in RMA", "MPI_Get", "MPI_Put", "MPI_Accumulate", "MPI_Win_fence"], (np.concatenate((overalls, self.opdurations[0:4]))).tolist())
 
 
 		winstats = np.stack((self.winsizes, self.xfer_per_win, self.epochs, self.windurations))
 		print('------------------------------------------------------------------------------------------\n' +
 		'---------------------------- Memory Windows ----------------------------------------------\n')
-		fp.forma_print_stats_x4(["Window sizes (B)", "Bytes transferred/win.", "Epochs per win.", "Window durations (nsec)"], winstats.tolist())
+		fp.forma_print_stats_x4(["Window sizes (B)", "Bytes transferred/win.", "Epochs per win.", "Window durations (usec)"], winstats.tolist(), 1)
 
 		
-		if fg.transfers == 1:
-			dtbounds_stats = []
-			for i in range(len(self.dtbounds)):
-				dtbounds_stats.append((self.dtbounds[i][0:4]).tolist())
-			print('------------------------------------------------------------------------------------------\n' +
-			'-------------------------- Data Transfer Bounds ------------------------------------------\n')
-			fp.forma_print_stats_x4(["MPI_Get", "MPI_Put", "MPI_Accumulate"], dtbounds_stats)
+		# if fg.transfers == 1:
+		dtbounds_stats = []
+		for i in range(len(self.dtbounds)):
+			dtbounds_stats.append((self.dtbounds[i][0:4]).tolist())
+		print('------------------------------------------------------------------------------------------\n' +
+		'-------------------------- Data Transfer Bounds ------------------------------------------\n')
+		fp.forma_print_stats_x4(["MPI_Get", "MPI_Put", "MPI_Accumulate"], dtbounds_stats, 0)
 		
 		return True
 
@@ -277,7 +277,7 @@ class epochSummary:
 		for i in range(len(self.opdurations)):
 			opduration_stats.append((self.opdurations[i][0:4]).tolist())
 		print('---------------------------- Operation Durations -------------------------------------------\n')
-		fp.forma_print_stats_x4(["MPI_Get", "MPI_Put", "MPI_Accumulate"], opduration_stats)
+		fp.forma_print_stats_x4(["MPI_Get", "MPI_Put", "MPI_Accumulate"], opduration_stats, 0)
 
 		
 		return True

forma_prints.py
@@ -48,7 +48,8 @@ def forma_print_stats_x6(row_labels, row_data):
 		# fl.forma_print(f'Shapes! row_labels: {np.shape(row_labels)}, row_data: {np.shape(row_data)}')
 		# fl.forma_print(f'len(row_labels): {len(row_labels)}')
 		# fl.forma_print(f'row_labels: {row_labels},\nrow_data: {row_data}')
-		rows = [[row_labels[i]]+row_data[i] for i in range(len(row_labels))]
+		#rows = [[row_labels[i]]+row_data[i] for i in range(len(row_labels))]
+		rows = [[row_labels[i]]+[x/1000.0 for x in row_data[i]] for i in range(len(row_labels))]
 	except TypeError:
 		fl.forma_error('forma_print_stats_x6: check row_labels and row_data types')
 		sys.exit(2)
@@ -58,12 +59,21 @@ def forma_print_stats_x6(row_labels, row_data):
 	return True
 	
 
-def forma_print_stats_x4(row_labels, row_data):
+def forma_print_stats_x4(row_labels, row_data, mode):
+
+	# mode == 0 means all data are nanoseconds and must be converted to usec
+	# mode == 1 means data are mixed: durations, counts, bytes, etc 
 
 	try:
-		rows = [[row_labels[i]]+row_data[i] for i in range(len(row_labels))]
+		if mode == 1:
+			for i in range(len(row_labels)):
+				if "usec" in row_labels[i]:
+					row_data[i] = [x/1000.0 for x in row_data[i]]
+			rows = [[row_labels[i]]+row_data[i] for i in range(len(row_labels))]
+		else: 
+			rows = [[row_labels[i]]+[x/1000.0 for x in row_data[i]] for i in range(len(row_labels))]
 	except TypeError:
-		fl.forma_print('forma_print_stats_x6: check row_labels and row_data types')
+		fl.forma_print('forma_print_stats_x4: check row_labels and row_data types')
 		sys.exit(2)
 
 	print(f'{tabulate(rows, headers=["aggregate", "min", "max", "avg"])}\n')